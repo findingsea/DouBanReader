30caaacd2192024a1092f79d144dc774214ebeb1
Lin Yin <findingsea@gmail.com>
2015-08-29 21:44:58 +0800

    fix report filename bug

--- template.py
+++ template.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-MARKDOWN_TEMPLATE_TITLE = u'%(year)d年%(year)d月阅读报告'
+MARKDOWN_TEMPLATE_TITLE = u'%(year)d年%(month)d月阅读报告'
 MARKDOWN_TEMPLATE_SECTION_TITLE = u'### [《%(title)s》](%(alt)s)'
 MARKDOWN_TEMPLATE_SECTION_PICTURE = u'![《%(title)s》](%(image)s)'
